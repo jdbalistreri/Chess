05cee7bde5fc83e1917a012a30ce97aaa839442c
joeandkarthik <joeandkarthik@appacademy.io>
2015-01-27 14:07:05 -0500

    implement knight subclasse's movement

--- chess/knight.rb
+++ chess/knight.rb
@@ -16,7 +16,21 @@ class Knight < SteppingPiece
     super
   end
 
+  def moves
 
+  end
 
+  def generate_deltas
+    possible_moves = []
+    curr_y, curr_x = @coordinates
 
+    DELTAS.each do |(dy, dx)|
+      new_y, new_x = curr_y + dy, curr_x + dx
+      next if [new_y, new_x].any? { |coord| !coord.between?(0,7) }
+
+      possible_moves << [new_y, new_x]
+    end
+
+    possible_moves
+  end
 end
--- chess/stepping_piece.rb
+++ chess/stepping_piece.rb
@@ -1,6 +1,6 @@
 require_relative("chess.rb")
 
-class SteppingPiece
+class SteppingPiece < Piece
 
   def initialize(board, color, coordinates)
     super
@@ -8,6 +8,10 @@ class SteppingPiece
 
   def moves(move_positions) #this will slide in each direction until invalid move
     #make sure we can take the opponent's piece
+  end
+
+  def check_deltas_against_board
+
   end
 
 end
