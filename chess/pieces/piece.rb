62162e8adddab5e87069b517ef0054fc189415c1
Joe Balistreri <jdbalistreri@gmail.com>
2015-03-26 17:15:50 -0400

    finish refactoring piece class

--- chess/pieces/piece.rb
+++ chess/pieces/piece.rb
@@ -2,25 +2,17 @@
 require_relative("../chess.rb")
 
 class Piece
-  attr_accessor :coordinates, :value
+  attr_accessor :coordinates, :value, :board
   attr_reader :color
-
-  PIECE_VALUES = { Pawn: 1,
-                   Knight: 3,
-                   Bishop: 3,
-                   Rook: 5,
-                   Rook: 5,
-                   Queen: 9 }
 
   def initialize(board, color, coordinates)
     @board, @color, @coordinates = board, color, coordinates
-    @value = PIECE_VALUES[self.class.to_s.to_sym]
   end
 
   def valid_moves
     self.moves.select do |move|
-      board_clone = @board.dup
-      board_clone.move!(coordinates,move)
+      board_clone = self.board.dup
+      board_clone.move!(self.coordinates,move)
 
       !board_clone.in_check?(self.color)
     end
@@ -33,25 +25,25 @@ class Piece
   end
 
   # UTILITY METHODS
+  def opposing_piece?(pos)
+    return false if empty_spot?(pos)
+    piece = self.board[pos]
+    self.white? ? piece.black? : piece.white?
+  end
+
+  def empty_spot?(pos)
+    self.board[pos].nil?
+  end
+
+  def on_the_board?(pos)
+    pos.all? { |el| el.between?(0,7) }
+  end
+
   def white?
     self.color == :white
   end
 
   def black?
     self.color == :black
-  end
-
-  def opposing_piece?(pos)
-    return false if empty_spot?(pos)
-    piece = @board[pos]
-    self.white? ? piece.black? : piece.white?
-  end
-
-  def empty_spot?(pos)
-    @board[pos].nil?
-  end
-
-  def on_the_board?(pos)
-    pos.all? { |el| el.between?(0,7) }
   end
 end
